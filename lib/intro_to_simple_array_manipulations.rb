666757b644a55cbe31841e2ba2a9c590dfb147d5
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-04 20:09:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,87 @@
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+next_country = "Niger"
+
+def using_push(array, string)
+   updated_array = array.push(string)
+   return updated_array
+end
+using_push(countries_in_western_africa, next_country)
+
+neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+
+def using_unshift(array, string)
+  new_neighborhood = "Brooklyn Heights"
+  updated_array = array.unshift(string)
+    return updated_array
+end
+
+great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+
+def using_pop(array)
+   deleted_string = array.pop
+   return deleted_string
+end
+
+chars_in_game_of_thrones= ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+
+def pop_with_args(array)
+   chars_arya_killed = array.pop(2)
+      return  chars_arya_killed
+end
+
+my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+
+def using_shift(array)
+   im_so_over_this_city = array.shift 
+     return im_so_over_this_city
+end
+
+ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
+def shift_with_args(array)
+  brands_removed = array.shift
+     return brands_removed
+end
+
+my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["mario kart", "flatiron school"]
+
+def using_concat(array1, array2)
+   all_my_favs = array1.concat([array2])
+     return all_my_favs
+end
+
+
+list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+another_esoteric_language = "Malbolge"
+
+def using_insert(array, string)
+   new_array = another_esoteric_language.insert 
+   another_esoteric_language.insert[4]
+     return new_array
+end
+
+def using_uniq
+   new_array = captain_planet_and_the_planeteers.uniq 
+     return new_array
+end
+
+def using_flatten
+   flat_array = private_colleges_in_newyork.flatten
+     return flat_array
+end
+
+def using_delete
+   instructors.delete 
+     return instructors
+end
+
+def using_delete_at
+   deleted_robot = famous_robots(2).delete 
+     return deleted_robot
+end
+  
+  
+  
+  
+   
